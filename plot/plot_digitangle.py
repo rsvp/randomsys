5d4e11d4df0e00a8100e059ebe9db2b48dd519d9
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2017-10-03 11:08:06 -0700

    plot/plot_digitangle.py: New origin_circle()
    Mark starting point and provide color compass as legend.

--- plot/plot_digitangle.py
+++ plot/plot_digitangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263 
-#        python 2.7.13 under Linux Ubuntu 14.04.5     Date : 2017-10-02
+#        python 2.7.13 under Linux Ubuntu 14.04.5     Date : 2017-10-03
 #  [/] - Cross-platform code compatible with python2.7 and python3.
 ''' 
 _______________|  plot_digitangle.py : Plot digits given their assigned angle.
@@ -13,16 +13,17 @@ _______________|  plot_digitangle.py : Plot digits given their assigned angle.
                   (HEATMAP) are determined by the input digits.
 
                   Thus this script visualizes any sequence of digits.
-                  If a circular angles are mapped on a random sequence we 
+                  If circular angles are mapped on a random sequence we 
                   will see a drunkard walk across the screen. 
                   Furthermore, if the sequence comes from a "normal" number, 
                   we can expect recurrent behavior, i.e. the drunk turtle 
                   will return to where it started its journey, though
                   it may take a long time to do so theoretically.
 
-                  The source of digits is taken to be a generator function 
+                  The source of digits can be a generator function 
                   (a random generator is furnished), or any text file 
-                  (e.g. pi-digits.txt containing 10,000 digits of pi).
+                  (e.g. pi-digits.txt containing 10,000 digits of pi)
+                  containing digits.
 
           Usage:  $ python plot_digitangle.py
                   ___ATTN___ Turtle may travel outside the screen boundary, 
@@ -40,6 +41,7 @@ _______________|  plot_digitangle.py : Plot digits given their assigned angle.
                   e  digits: https://apod.nasa.gov/htmltest/gifcity/e.1mil
 
 CHANGE LOG  For latest changes, see https://github.com/rsvp/randomsys
+2017-10-03  Add origin_circle() marking starting point and color compass.
 2017-10-02  First version is a complete revision of:
                https://gist.github.com/cavedave/423d67a583ad10925aa6dc85ab7acab4
                using Python idioms and heatmap for colors.
@@ -122,8 +124,22 @@ def get_randigit():
     yield random.randint(0, 9)
 
 
+def origin_circle( pixels=210 ):
+    '''Mark starting point and provide color compass as legend.'''
+    for digit in range(0, 10):
+        angle = digit * 36
+        turtle.left(angle)
+        turtle.forward(pixels)
+        turtle.pencolor('black')
+        turtle.write(str(angle) + '° ' + HEATMAP[digit] + '=' + str(digit))
+        turtle.pencolor(HEATMAP[digit])
+        turtle.forward(-pixels)
+        turtle.left(-angle)
+
+
 def run_circle( genfun, pixels=5, iterations=43 ):
     '''Use d2circle to visualize drunkard walk.'''
+    origin_circle()
     for i in range( iterations ):
         digit = next(genfun())
         turtle.pencolor(HEATMAP[digit])
@@ -132,6 +148,7 @@ def run_circle( genfun, pixels=5, iterations=43 ):
 
 def run_circle_read( filename, pixels=5 ):
     '''Use d2circle to visualize drunkard walk reading a file.'''
+    origin_circle()
     for digit in read_digits(filename):
         turtle.pencolor(HEATMAP[digit])
         d2circle( digit, pixels )
@@ -145,24 +162,24 @@ def savework():
     turtle.bye()
 
 
-def main(case='random', datafile=None, save=False):
-    '''Healthy case values are: 'demo', 'pi', 'read', or 'random'.'''
+def main(case='randigit', datafile=None, save=False):
+    '''Healthy case values are: 'demo', 'randigit', 'pi', or 'read'.'''
     if case == 'demo':
-        run_circle(get_randigit, pixels=19, iterations=100)
-        #  Demo uses constant variables to slow and show work in progress.
+        #  Demo uses CONSTANT variables to SLOW work in progress.
+        random.seed(42)  # For reproducibility.
+        run_circle(get_randigit, pixels=42, iterations=50)
+    elif case == 'randigit':
+        wn.tracer( 100, delay=1) # Reasonable speed-up.
+        run_circle(get_randigit, pixels=4, iterations=10000)
     elif case == 'pi':
         wn.tracer( 100, delay=1) # Reasonable speed-up.
         run_circle_read('pi-digits.txt', pixels=4)
     elif case == 'read':
         wn.tracer( 100, delay=1) # Reasonable speed-up.
         run_circle_read(datafile, pixels=4)
-    elif case == 'random':
-        wn.tracer( 100, delay=1) # Reasonable speed-up.
-        #  random.seed(42)  # For reproducibility.
-        run_circle(get_randigit, pixels=4, iterations=10000)
     else:
         turtle.bye
-        raise ValueError("Please review main() for correct case.")
+        raise ValueError("Invalid case argument: review main().")
 
     if save:
         savework()
@@ -171,6 +188,6 @@ def main(case='random', datafile=None, save=False):
 
 
 if __name__ == "__main__":
-    main('random')
+    main('randigit')
 
 
